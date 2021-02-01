e470d227e4da0c6738f9c3bed4afc94e775ad5df
ekillham <ekillham@luc.edu>
2021-01-31 21:01:41 -0600

    Create list.c
    place holder for when I figure out linked lists :)

---
+++ src/list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "list.h"
+/* work in progress */
+
+void list_add (struct list_element *head, struct list_element* ele)
+{
+ return 0; 
+}
+
+void list_remove(structu list_element** head, int data)
+{
+ return 0;
+}
